4ab061616851a63ab92f16c763cb590045279bf8
ToSPP <esau@yandex.ru>
2019-11-28 14:17:05 +0300

    Lesson 3

--- controllers/ActivityController.php
+++ controllers/ActivityController.php
@@ -10,14 +10,13 @@ namespace app\controllers;
 
 
 use app\models\Activity;
+use app\models\ActivityForm;
 use yii\web\Controller;
 
 class ActivityController extends Controller
 {
     public function actionIndex()
     {
-
-//        return "Контроллер активностей";
         $model = new Activity();
         return $this->render('index', [
           'model' => $model
@@ -26,7 +25,49 @@ class ActivityController extends Controller
 
     public function actionCreate()
     {
-        return "Создание активности";
+        return $this->render('create', ['model' => new ActivityForm()]);
     }
 
+    public function actionUpdate()
+    {
+        return "Редактирование активности";
+    }
+
+    public function actionSubmit()
+    {
+        $model = new ActivityForm();
+
+        if (\Yii::$app->request->isPost) {
+            $model->save();
+        }
+
+        $errors = $model->getErrors();
+
+        return $errors ? $this->render('submit', ['errors' => $errors]) :
+            $this->render('submit', ['model' => $model]);
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
